d46468650db9ab13a1386c7e7ee276486bea7e55
murakami <halpygg@gmail.com>
2023-07-10 16:43:09 +0900

    update

--- app/src/main/java/com/example/recognizespeechapp/MainActivity.java
+++ app/src/main/java/com/example/recognizespeechapp/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.recognizespeechapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,32 +29,139 @@ public class MainActivity extends AppCompatActivity{
     private Button button;
     private static final int REQUEST_CODE = 0;
 
+    private final static String TAG = "SPEECH_RECOGNIZER";
+
+    public int count = 0;
+    private SpeechRecognizer speechRecognizer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.editTextView);
 
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                    intent.putExtra(
-                            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-                    );
-                    intent.putExtra(
-                            RecognizerIntent.EXTRA_PROMPT,
-                            "RecognizeSpeechApp"
-                    );
-                }catch (ActivityNotFoundException e){
-                    Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+                if(count == 0){
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                    count = 1;
+                }else {
+                    speechRecognizer.stopListening();
+                    count = 0;
                 }
+
             }
         });
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+                Log.i(TAG, "onReadyForSpeech");
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Log.i(TAG, "onBeginningOfSpeech");
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+                Log.i(TAG, "onRmsChanged");
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+                Log.i(TAG, "onBufferReceived");
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                Log.i(TAG, "onEndOfSpeech");
+            }
+
+            @Override
+            public void onError(int i) {
+                switch (i) {
+                    case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                        Log.d(TAG, "ネットワークタイムエラー");
+                        break;
+                    case SpeechRecognizer.ERROR_NETWORK:
+                        Log.d(TAG, "その外ネットワークエラー");
+                        break;
+                    case SpeechRecognizer.ERROR_AUDIO:
+                        Log.d(TAG, "Audio エラー");
+                        break;
+                    case SpeechRecognizer.ERROR_SERVER:
+                        Log.d(TAG, "サーバーエラー");
+                        break;
+                    case SpeechRecognizer.ERROR_CLIENT:
+                        Log.d(TAG, "クライアントエラー");
+                        break;
+                    case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                        Log.d(TAG, "何も聞こえてないエラー");
+                        break;
+                    case SpeechRecognizer.ERROR_NO_MATCH:
+                        Log.d(TAG, "適当な結果を見つけてませんエラー");
+                        break;
+                    case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                        Log.d(TAG, "RecognitionServiceが忙しいエラー");
+                        break;
+                    case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                        Log.d(TAG, "RECORD AUDIOがないエラー");
+                        break;
+                }
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                Log.i(TAG, "onResults");
+                ArrayList<String> data = results.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
+                editText.setText(data.get(0));
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+                Log.i(TAG, "onPartialResults");
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+                Log.i(TAG, "onEvent");
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResult) {
+        super.onRequestPermissionsResult(requestCode, permission, grantResult);
+        if (requestCode == 1) {
+            if (grantResult[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
